5f7f627c28caa8aa1ed85f675b5dece8202c7299
Georgy Kovalenko <b@tcoh.ru>
2020-04-23 12:42:12 +0300

    fix: remove unused property

--- src/generators/XKCDPasswordGenerator.js
+++ src/generators/XKCDPasswordGenerator.js
@@ -3,17 +3,12 @@ import React from "react";
 import { generateMnemonic } from "bip39";
 
 class XKCDPasswordGenerator extends BaseGenerator {
-  ready = false;
   title = (<a href="https://xkcd.com/936/">XKCD Password</a>);
   description = "Cool and memorable passphrase.";
 
   generate() {
     const mnemonic = generateMnemonic();
-    return mnemonic
-      .trim()
-      .split(/\s+/g)
-      .slice(0, 4)
-      .join("-");
+    return mnemonic.trim().split(/\s+/g).slice(0, 4).join("-");
   }
 }
 
