eda2780cfdb510fbfe4dc702d39e20b375a9d875
Georgy Kovalenko <b@tcoh.ru>
2020-10-12 02:05:54 +0300

    fix: clipboard button error state

--- src/components/clipboard/Clipboard.js
+++ src/components/clipboard/Clipboard.js
@@ -1,37 +1,39 @@
 import "./Clipboard.css";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 
-class Clipboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-    this.resetIcon = this.resetIcon.bind(this);
-    this.state = { icon: "clipboard" };
-  }
-  onClick() {
-    navigator.clipboard.writeText(this.props.value);
-    this.setState({ icon: "clipboard-check" });
-    setTimeout(this.resetIcon, 3000);
-  }
-  resetIcon() {
-    this.setState({ icon: "clipboard" });
-  }
-  render() {
-    if ("clipboard" in navigator) {
-      return (
-        <button
-          className="Clipboard"
-          onClick={this.onClick}
-          title="Copy to clipboard"
-        >
-          <FontAwesomeIcon icon={this.state.icon} />
-        </button>
-      );
-    }
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Clipboard = ({ value }) => {
+  const isClipboardSupported = "clipboard" in navigator;
+  const [icon, setIcon] = useState("clipboard");
+  const [isError, setIsError] = useState(false);
+
+  const onClick = () => {
+    navigator.clipboard
+      .writeText(value)
+      .then(() => {
+        setIcon("clipboard-check");
+        setTimeout(() => setIcon("clipboard"), 3000);
+      })
+      .catch(() => {
+        setIsError(true);
+        setTimeout(() => setIsError(false), 3000);
+      });
+  };
+
+  if (!isClipboardSupported) {
     return null;
   }
-}
+  return (
+    <button
+      className={"Clipboard" + (isError ? " Clipboard__error" : "")}
+      onClick={onClick}
+      title="Copy to clipboard"
+    >
+      <FontAwesomeIcon icon={icon} />
+    </button>
+  );
+};
 
 export default Clipboard;
