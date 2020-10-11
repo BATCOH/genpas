1dcbb1dd141b4c04c3ff08730a6d7fcb5003b51b
Georgy Kovalenko <b@tcoh.ru>
2020-10-12 02:07:19 +0300

    fix: pssword input class when the clipboard is not available

--- src/components/password/Password.js
+++ src/components/password/Password.js
@@ -1,40 +1,39 @@
 import "./Password.css";
 
+import React, { useRef } from "react";
+
 import AutoHeightTextArea from "../autoHeightTextArea";
 import Clipboard from "../clipboard";
-import React from "react";
 
-class Password extends React.Component {
-  constructor(props) {
-    super(props);
-    this.domRef = React.createRef();
-    this.onFocus = this.onFocus.bind(this);
-  }
-  onFocus() {
+const Password = ({ pass }) => {
+  const isClipboardSupported = "clipboard" in navigator;
+  const className =
+    "Password" + (isClipboardSupported ? " Password__hasClipboard" : "");
+
+  const domRef = useRef(null);
+
+  const onFocus = () => {
+    // Timeout to avoid Safari bug
     setTimeout(() => {
-      this.domRef.current.select();
+      domRef && domRef.current && domRef.current.select();
     }, 100);
-  }
+  };
 
-  render() {
-    const pass = this.props.pass;
-    if (!pass) return null;
-    const className =
-      "Password" + ("clipboard" in navigator && " Password__hasClipboard");
-    return (
-      <div className={className}>
-        <AutoHeightTextArea
-          className="Password-input"
-          value={pass}
-          readOnly={true}
-          ref={this.domRef}
-          onFocus={this.onFocus}
-          style={{ width: pass.length + ".1ch" }}
-        />
-        <Clipboard value={pass} />
-      </div>
-    );
-  }
-}
+  if (!pass) return null;
+
+  return (
+    <div className={className}>
+      <AutoHeightTextArea
+        className="Password-input"
+        value={pass}
+        readOnly={true}
+        ref={domRef}
+        onFocus={onFocus}
+        style={{ width: pass.length + ".1ch" }}
+      />
+      <Clipboard value={pass} />
+    </div>
+  );
+};
 
 export default Password;
