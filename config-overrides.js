667ada6c0752d6092121174c4ac14b68d3b8b178
Georgy Kovalenko <b@tcoh.ru>
2022-02-19 10:49:12 +0300

    ⬆️ react-scripts 5

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,25 @@
 const webpack = require("webpack");
 
-function ignoreBip39Wordlists(config, env) {
-  config.plugins.push(
-    new webpack.IgnorePlugin(/^\.\/wordlists\/(?!english)/, /bip39\/src$/)
-  );
-  return config;
-}
-
 module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...(config.resolve.fallback ?? {}),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer"),
+  };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
   if (env === "production") {
-    config = ignoreBip39Wordlists(config, env);
+    // ignore bip39 wordlists
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/wordlists\/(?!english)/,
+        contextRegExp: /bip39\/src$/,
+      })
+    );
   }
   return config;
 };
